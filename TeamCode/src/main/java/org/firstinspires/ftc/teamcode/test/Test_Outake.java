1de76753a9b0067fe090221aacb1aed5f53831a2
Na Zyia Nelson <324641@my.puhsd.org>
2021-10-07 17:21:41 -0700

    wrote outake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/Test_Outake.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.test;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.Outake;
+
+@Autonomous (name= "Outake test")
+public class Test_Outake  extends LinearOpMode {
+    Outake outake;
+    @Override
+    public void runOpMode () throws InterruptedException {
+        outake = new Outake(hardwareMap.crservo.get("outake"));
+        waitForStart();
+
+        outake.moveForward( 0.5);
+        sleep( 5000)
+        outake.moveBack( 0.5 );
+        sleep(5000);
+        outake.stop();
+        sleep(5000)
+    }
+}
