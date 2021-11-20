eb43bce2318a9926112d8266cfd271ae9990c6cb
Agrocrab <90426692+Agrocrab@users.noreply.github.com>
2021-11-19 17:29:24 -0800

    BACKLEFT BLUE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/slimeAutonBL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/slimeAutonBL.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.auton;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.hardware.Constants;
 import org.firstinspires.ftc.teamcode.hardware.Robot2;
 
+@Autonomous (name = "slimeAutonBL", group = "PRTest")
 public class slimeAutonBL extends LinearOpMode {
 
 Robot2 robot = new Robot2();
@@ -11,22 +14,26 @@ Robot2 robot = new Robot2();
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap, telemetry);
-        robot.drivetrain.setPower(.5);
+        robot.drivetrain.setPower(.4);
+        robot.drivetrain.setTelemetry(telemetry);
+        robot.drivetrain.useBrake(true);
         waitForStart();
         while(opModeIsActive()) {
             // forward x inches
-            robot.drivetrain.forward(24);
-            // stop
-            robot.drivetrain.stop();
+            robot.drivetrain.forward(16);;
             sleep(500);
             // turn left 90 degrees
-            robot.drivetrain.pointTurnLeft();
+            robot.drivetrain.pointTurn(Constants.Status.RIGHT, 14.1);
+            //backwards
+            robot.drivetrain.forward(8);
+            sleep(600);
+            // forward
+            robot.drivetrain.setPower(.9);
+            robot.drivetrain.backward(75);
             // stop
             robot.drivetrain.stop();
-            // forwad x inches
-            robot.drivetrain.forward(12);
-            // stop
-            robot.drivetrain.stop();
+
+            break;
 
 
         }
